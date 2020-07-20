d72f90fb3fc5ead7abdf61742506f67de2055dfd
apzivile <zivile.apo@gmail.com>
2020-07-20 19:52:54 +0300

    Game of life project; finished 1st part

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,7 +1,32 @@
 package life;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    private static Random random;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int seed = scanner.nextInt();
+
+        random = new Random(seed);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (isAlive()) {
+                    System.out.print("O");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean isAlive() {
+        return random.nextBoolean();
     }
 }
